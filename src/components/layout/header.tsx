70ef37aa464d4110d8f8cb6695b1ed089d141c8e
Marlon <marlon.deocampo@shipteksolutions.com>
2023-01-28 16:46:36 +0800

    added cart

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,8 +1,13 @@
+import Carticon from "../svg/cart-icon";
+
 const Header = () => {
     return (
         <div className="flex justify-between items-center h-14 px-10 bg-primary text-white">
             <div>My Exam</div>
-            <div>Cart</div>
+            <div className="flex flex-row space-x-2 cursor-pointer">
+                <p>Cart</p>
+                <Carticon />
+            </div>
         </div>
     );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,17 +5,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      body: ["Nunito Sans"],
+    },
     extend: {
       colors: {
         "primary": "#17392d",
         "secondary": "#87eaa7"
       },
       gridTemplateColumns: {
-        // Simple 16 column grid
-        '16': 'repeat(16, minmax(0, 1fr))',
-
-        // Complex site-specific column configuration
-        'footer': '200px minmax(900px, 1fr) 100px',
         'cards': 'repeat(auto-fill, 250px)'
       }
     },
