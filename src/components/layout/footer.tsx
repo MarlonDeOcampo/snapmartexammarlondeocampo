572b3afcd28be313449cc26234bfa6682f605abc
Marlon <marlon.deocampo@shipteksolutions.com>
2023-01-29 19:58:43 +0800

    added dist2

--- src/components/layout/footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer: React.FC = () => {
-    return (
-        <div className="fixed bottom-0 w-full flex justify-center items-center h-14 px-10 bg-primary text-white">
-            <div>All Rights Reserved </div>
-        </div>
-    );
-};
-
-export default Footer;
--- src/components/products.tsx
+++
@@ -1,7 +0,0 @@
-const Products: React.FC = () => {
-    return (
-        <div>this is products</div>
-    );
-};
-
-export default Products;
